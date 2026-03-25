fef3bc36bc2107cf31c02a9dbfc0173736f09f0e
Noki <denenoch.mendoza@gmail.com>
2026-03-25 21:47:10 +0800

    change the delete button to icon

--- script.js
+++ script.js
@@ -857,11 +857,11 @@ if (window.location.pathname.includes('cart.html')) {
         closeSvg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
         closeSvg.setAttribute('height', '32px');
         closeSvg.setAttribute('viewBox', '0 -960 960 960');
-        closeSvg.setAttribute('width', '32px');
+        closeSvg.setAttribute('width', '22px');
         closeSvg.setAttribute('fill', '#5f6368');
         
         const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-        path.setAttribute('d', 'm256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z');
+        path.setAttribute('d', 'M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z');
         
         closeSvg.appendChild(path);
         closeInfoDiv.appendChild(closeSvg);
